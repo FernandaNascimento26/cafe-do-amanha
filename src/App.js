c06be051e44c5eddcce2fa85d19229feb34553d1
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2024-08-18 21:17:18 -0300

    Enviado o projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Stories from './components/Stories';
+import Contact from './components/Contact';
+import Menu from './components/Menu';
+import OrderPage from './components/OrderPage';
+import NotFound from './components/NotFound';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Hero />} />
+                <Route path="/sobre-nos" element={<About />} /> 
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/pedidos" element={<OrderPage />} />
+                <Route path="/historias" element={<Stories />} />  
+                <Route path="/contato" element={<Contact />} />  
+                <Route path="*" element={<NotFound />} /> 
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
